b5bb6f669b35753d81fd7ba85940a680b99ef04c
chbuescher <Christoph.Buescher@fiduciagad.de>
2016-11-15 17:00:50 +0100

    issue #216: use libevent for socket operations

--- server.c
+++ server.c
@@ -258,7 +258,7 @@ server_queuereader(void *d)
 						logerr("failed to create pipe: %s\n", strerror(errno));
 					continue;
 				}
-				dispatch_addconnection(intconn[0]);
+				dispatch_addlistener(intconn[0], 1);
 				self->fd = intconn[1];
 			} else if (self->ctype == CON_FILE) {
 				if ((self->fd = open(self->ip,
