556f501a37bbcae4ae3f2abbbe5c4ad498c22298
Vladimir Smirnov <vladimir.smirnov@booking.com>
2017-12-07 15:46:48 +0100

    Revert "optimize dispatch_parsebuf"
    This reverts commit faf56b32d0f672e7576dc7538601aeca1243e035.

--- dispatcher.h
+++ dispatcher.h
@@ -54,6 +54,5 @@ void dispatch_accept_cb_exp(int fd, short ev, void *arg);
 void dispatch_accept_cb_noexp(int fd, short ev, void *arg);
 void dispatch_initlisteners(void);
 void dispatch_destroylisteners(void);
-void dispatch_initctype(char *c);
 
 #endif
